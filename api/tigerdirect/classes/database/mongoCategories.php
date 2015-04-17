b29d08f0142308f303793a97b532803694382f63
Dave Holowiski <david@holowiski.com>
2015-04-17 07:54:48 -0600

    more work on api build out of mongo query class

---
+++ api/tigerdirect/classes/database/mongoCategories.php
@@ -0,0 +1,70 @@
+<?php
+
+class mongoCategories {
+	const MONGO_TABLE_TD_CATEGORY = "td_category";
+
+	const MONGO_ITEM_TYPE_FIELD = "itemType";
+	const MONGO_CATEGORY_FIELD = "category";
+	#define("MONGO_ITEM_TYPE_FIELD", "itemType");
+	#define("MONGO_CATEGORY_FIELD", 'category');
+
+	private $serverIP;
+	private $serverPort;
+	private $database;
+	private function setServerIP($ip) { $this->serverIP = $ip; }
+	private function getServerIP() { return $this->serverIP; }
+	private function setServerPort($port) { $this->serverPort = $port; }
+	private function getServerPort() { return $this->serverPort; }
+	private function setDatabase($database) { $this->database = $database; }
+	private function getDatabase() { return $this->database; }
+
+	private $mongoClient;
+	private function setMongoClient($client) { $this->mongoClient = $client; }
+	private function getMongoClient() { return $this->mongoClient; }
+	private $mongoDb;
+	private function setMongoDB($db) { $this->mongoDB = $db; }
+	private function getMongoDB() { return $this->mongoDB; }
+
+	public function __construct($serverIP, $serverPort, $database) {
+		$this->setServerIP($serverIP);
+		$this->setServerPort($serverPort);
+		$this->setDatabase($database);
+
+		#should surround in a try/catch
+		$conn = new MongoClient("mongodb://".$this->getServerIP().":".$this->getServerPort());
+		$db = $conn->{$this->getDatabase()};
+		$this->setMongoClient($conn);
+		$this->setMongoDB($db);
+
+	}
+
+	function __destruct() {
+       echo "Destroying mongoCategories\n";
+   }
+
+	public function getCategoryByMongoID($mongoID) {
+		/*Gets a single category, by it's mongoID
+		Expects a data type MongoID
+		Returns an associative array which is the category object*/
+
+		$mongoCat = $this->getMongoDB();
+		#$mongoID = new MongoID($mongoID);
+		$document = $mongoCat->findOne(array('_id' => $mongoID));
+
+		return $document;
+
+	}
+
+	public function getCategories($query) {
+
+	}
+
+	public function buildQuery($stuff, $things) {
+		$query = array();
+
+		return $query;
+	}
+
+}
+
+?>
--- api/tigerdirect/getProductCategories.php
+++ api/tigerdirect/getProductCategories.php
@@ -8,14 +8,26 @@
 		anyway, mostly i'll be searching by id. but sometimes name.
 */
 
+
 require_once("../shared/mongoConfig.php");
+require_once(__DIR__."/classes/database/mongoCategories.php");
+
 #TODO: error checking! robust!
-$m = new MongoClient("mongodb://".MONGO_SERVER_IP.":".MONGO_SERVER_PORT);
-$db = $m->{MONGO_DATABASE};
-$collectionCategory = $db->{MONGO_TABLE_TD_CATEGORY};
+//$m = new MongoClient("mongodb://".MONGO_SERVER_IP.":".MONGO_SERVER_PORT);
+//$db = $m->{MONGO_DATABASE};
+//$collectionCategory = $db->{MONGO_TABLE_TD_CATEGORY};
 
 #for now, just find one. set up to accept params
-$document = $collectionCategory->findOne(array('_id' => new MongoId('55281a158a1d5769fea76945')));
-var_dump( $document );
+
+//$query = array(MONGO_ITEM_TYPE_FIELD=>MONGO_CATEGORY_FIELD);
+
+//$document = $collectionCategory->findOne($query);
+//$document = $collectionCategory->findOne(array('_id' => new MongoId('5527d83c4184797db0b15cde')));
+//var_dump( $document );
+
+$abc = new mongoCategories(MONGO_SERVER_IP, MONGO_SERVER_PORT, MONGO_DATABASE);
+$mongoID = new MongoId('5527d83c4184797db0b15cde');
+
+var_dump($abc->getCategoryByMongoID($mongoID));
 
 ?>
